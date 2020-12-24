9443d7acbc451ca5974f7edcc6a6091c9dd93d8a
jihwanK <jhwan1228@gmail.com>
2020-12-24 02:15:05 +0000

    update us_election

--- us_election/election.py
+++ us_election/election.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import os
 
-elections = pd.read_csv('./election/president_county_candidate.csv')
+us_election_path = os.path.join(os.getcwd(), 'us_election')
+file_path = os.path.join(us_election_path, 'election', 'president_county_candidate.csv')
+elections = pd.read_csv(file_path)
+
 print(elections.county.value_counts())
