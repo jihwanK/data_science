6d4cf27788b74fd09c00e4624a4f657eba166bf3
jihwanK <jhwan1228@gmail.com>
2021-08-23 21:32:41 +0900

    update for machine learning practice

---
+++ machine_learning/oecd_bli.py
@@ -0,0 +1,15 @@
+import sklearn
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import pandas as pd
+
+oecd_df = pd.read_csv('oecd_bli.csv', thousands=',')
+gdp_df = pd.read_csv('gdp_per_capita.tsv', sep='\t', thousands=',', encoding='latin1', na_values='n/a')
+
+print(oecd_df)
+print(oecd_df.describe())
+print(gdp_df)
+print(gdp_df.describe())
+
+# country_stats = prepare_country_stats()
