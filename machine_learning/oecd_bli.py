b1275703ef9ebb9681b1257116ac811e66ea6903
Jihwan Kim <jhwan1228@gmail.com>
2021-08-24 13:43:03 +0900

    update file and add gdp files

--- machine_learning/oecd_bli.py
+++ machine_learning/oecd_bli.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 
 oecd_df = pd.read_csv('oecd_bli.csv', thousands=',')
-gdp_df = pd.read_csv('gdp_per_capita.tsv', sep='\t', thousands=',', encoding='latin1', na_values='n/a')
+# gdp_df = pd.read_csv('gdp_per_capita.tsv', sep='\t', thousands=',', encoding='latin1', na_values='n/a')
+gdp_df = pd.read_csv('gdp.tsv', sep='\t', thousands=',', encoding='latin1', na_values='n/a')
 
 print(oecd_df)
 print(oecd_df.describe())
